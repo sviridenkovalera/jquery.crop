6f5503d32bff4a70ccfd4e38810742552ecda9cd
valera <cowr.valera@gmail.com>
2014-10-15 04:00:21 +0400

    improve crop algorithm

--- js/jquery.crop-0.1.1.js
+++ js/jquery.crop-0.1.1.js
@@ -2,23 +2,24 @@
     "use strict";
 
     $.fn.crop = function (options) {
-        var image = $(this),
+        var image = $('img').clone(),
             parent = $('<div>'),
-            insertParent = image.parent(),
             left = Math.floor(options.left) || 0,
             top = Math.floor(options.top) || 0,
             width = Math.floor(options.width) || Math.floor(image.width()),
             height = Math.floor(options.height) || Math.floor(image.height());
 
-        image.remove();
-        image.css('left', -left + "px");
-        image.css('top', -top + "px");
-        image.css('position', 'relative');
         parent.css('overflow', 'hidden');
         parent.css('width', width + "px");
         parent.css('height', height + "px");
+        parent.css('display', "inline-block");
         image.appendTo(parent);
-        parent.appendTo(insertParent);
+        $(this).before(parent);
+        $(this).remove();
+
+        image.css('left', -left + "px");
+        image.css('top', -top + "px");
+        image.css('position', 'relative');
         return image;
     };
 
